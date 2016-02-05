930f77a8c1a8d1727633c885c6abb4bae73fb1cd
Yin Gang <zenith.yin@gmail.com>
2016-02-05 14:04:38 +0800

    Level 5

---
+++ 05.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+import pickle
+
+if __name__ == "__main__":
+    f = open('05.pickle', 'rb')
+    d = pickle.load(f)
+    for row in d:
+        for column in row:
+            print(column[0]*column[1], end='')
+        print()

cbee049eb8e14b87d4037f92852dd71d6230ff0e
Yin Gang <zenith.yin@gmail.com>
2016-02-05 10:06:51 +0800

    EXACTLY

--- 03.py
+++ 03.py
@@ -3,12 +3,12 @@ from __future__ import absolute_import, division, print_function
 import re
 
 def filter(filename):
-    rule = '[A-Z]{3}[a-z]{1}[A-Z]{3}'
+    rule = '[a-z]{1}[A-Z]{3}[a-z]{1}[A-Z]{3}[a-z]{1}'
     for line in open(filename):
         result = re.findall(rule, line)
         for item in result:
-            if len(item) > 3:
-                print(item[3], end="")
+            if len(item) > 4:
+                print(item[4], end="")
             
 if __name__ == "__main__":
     filter('03.txt')

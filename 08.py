c2e0d8cf072918c264e297160ff6d11d82e7c5f0
Yin Gang <zenith.yin@gmail.com>
2016-02-06 12:18:24 +0800

    Level 08

---
+++ 08.py
@@ -0,0 +1,7 @@
+#from __future__ import absolute_import, division, print_function
+
+import bz2
+
+if __name__ == "__main__":
+    print(bz2.decompress('BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'))
+    print(bz2.decompress('BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'))

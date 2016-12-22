d333ed7446c00b56e5b88780441ec8b589271961
Yin Gang <zenith.yin@gmail.com>
2016-12-22 10:29:34 +0800

    fix 05.py for file opening

--- 05.py
+++ 05.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import pickle
 
 if __name__ == "__main__":
-    f = open('05.pickle', 'rb')
+    f = open('05.pickle', 'r')
     d = pickle.load(f)
     for row in d:
         for column in row:
